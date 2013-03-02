aeb5efc0512e3c76483e3e89f6d445be74d3e7ef
Adam Haapala <adamhaapala@yahoo.com>
2013-03-02 18:41:45 -0500

    derp missed some files

--- fallout-hacking-game/config.sh
+++ fallout-hacking-game/config.sh
@@ -1,5 +1,32 @@
 #!/bin/sh
 
-PASSWORDLIST=()
-DIFFICULTY=EASY      # EASY, HARD, VERYHARD, OHFUCKTHIS
-NUMBEROFTRIES=4      # Default in the game
+## User Config and Globals
+## not likely to change but 
+## who knows
+
+PASSWORDLIST=()         # 1-dim array of possible passwords
+DIFFICULTY=EASY         # EASY, HARD, VERYHARD, OHFUCKTHIS
+NUMBEROFTRIES=4         # Default in the game
+HISTORY=""              # Array to contain history 
+TERMROW=${tput lines}	# The terminal rows
+TERMCOL=${tput cols}    # and cols, good to know...
+
+# Header for hacking game
+TERMHEADER= read -d '' TERMHEADER << "EOF"
+ROBOCO INDUSTRIES (TM) TERMLINK PROTOCOL
+ENTER PASSWORD NOW
+EOF
+
+# Keys to match while in game
+UP='^[[A'
+DOWN='^[[B'
+LEFT='^[[D'
+RIGHT='^[[C'
+ENTER=''
+ESC=''
+
+ATTEMPTS=4 # Number of active attempts
+Declare -a SOLUTION   # Two-dimensional array containing solution to puzzle
+X=""	# Col location of cursor
+Y=""    # Row location of cursor
+
--- fallout-hacking-game/hacking.sh
+++ fallout-hacking-game/hacking.sh
@@ -5,54 +5,31 @@
 # Pull in config file
 ./config.sh
 
-ATTEMPTS=0 # Number of active attempts
-KEY=(())   # Two-dimensional array containing solution to puzzle
-CURSOR=""
-
 ## Sub-routines ##
+./util.sh
 
-sub randomElement (){
-    array=$1
-    list_length=#$array
-    return $[( $RANDOM % $list_length )+1]
-}
-
-# This gets the password for this session of the game
-sub selectPass(){
-   if [ $PASSWORDLIST ] 
-   then 
-       list_length=#$PASSWORDLIST
-       ran_num=randomElement($list_length)
-       return $PASSWORDLIST[$ran_num]
-   fi
-   return -1
-}
-
-# Generates a 10x20 field of random characters
-sub generateRandomField(){
-
-
-}
-
-# Inserts a number of words based on difficulty 
-sub placePasswords(){
-
-}
-
-# Moves the cursor to the passed grid location
-sub moveCursor(){}
-
-# Main loop 
 # only ctrl-c or winning/loosing the game will exit
 sub play(){
 
-    CURSOR="0,0"
-    moveCursor($CURSOR)
+    # Game's started
+    # Assuming screen is drawn begin taking input
+    # Cursor should start and stay in the two panels
+    # of random elements.  See off-sets for panel
+    # definitions
+    X="0"
+    Y="0"
+    moveCursor($X,$Y)
     while [ 1 ]
     do
         read -n 1 key
         case "$key" in
-
+	  $UP)arrowUp(1);;
+	  $DOWN)arrowDown(1);;
+	  $LEFT)arrowLeft(1);;
+          $RIGHT)arrowRight(1);;
+	  $ENTER)checkCell();
+          *)	# Catch all
+		;;
         esac
     done
 }
@@ -67,3 +44,4 @@ play($GRID)
 
 trap 'exit 1' INT TERM
 trap 'tput setaf 9; tput cvvis; clear' EXIT
+
